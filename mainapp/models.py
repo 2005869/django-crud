741ac26c93f4bacf907324dc67a61bdfdc2346b6
2005869 <2005869@aluno.univesp.br>
2023-04-11 18:41:35 -0300

    model to use and migrations

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class NameModel(models.Model):
+    name = models.CharField('Name', max_length=50)
+    age = models.PositiveIntegerField(max_length=3)
