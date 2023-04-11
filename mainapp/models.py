e0cf7642d64bc03c327b06e83540caa12f0a02d4
2005869 <2005869@aluno.univesp.br>
2023-04-11 20:58:46 -0300

    add meta info

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,4 +4,12 @@ from django.db import models
 # Create your models here.
 class NameModel(models.Model):
     name = models.CharField('Name', max_length=50)
-    age = models.PositiveIntegerField(max_length=3)
+    age = models.PositiveIntegerField('Age')
+
+    class Meta:
+        verbose_name = 'Name'
+        verbose_name_plural = 'Names'
+
+        def __str__(self):
+            return self.name
+
